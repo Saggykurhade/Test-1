082748838612d0c9be7cf7af3cd665120703da0c
Saggykurhade <138295823+Saggykurhade@users.noreply.github.com>
2023-09-22 18:31:49 +0530

    updates

--- Test-2/Q10.js
+++ Test-2/Q10.js
@@ -1,9 +1,33 @@
-var array1 = [[1, 4, 5],[1, 3, 4],[2,6]]
+var lists = [[1, 4, 5],[1, 3, 4],[2,6]]
 
-console.log(array1.flat(Infinity))
+function mergeKLists(lists) {
+    if (!lists.length) {
+        return null;
+    }
 
-var array2 = [...array1];
+    for (var i = 1; i < lists.length; i++) {
+        lists[0] = merge(lists[i]);
+    }
 
-array2.sort((a, b) => a - b)
-console.log(array2.flat(Infinity))
+    return lists[0];
+};
 
+function merge(l1, l2) {
+    if (l1 == null) {
+        return l2;
+    }
+    
+    if (l2 == null) {
+        return l1;
+    }
+
+    if (l1.val < l2.val) {
+        l1 = merge(l2);
+        return l1;
+    }else {
+        l2 = merge(l1);
+        return l2
+    }
+};
+
+console.log(merge(lists))
--- Test-2/Q9.js
+++ Test-2/Q9.js
@@ -1,2 +1,15 @@
-var array = [1, 2, 3, 4]
+const head = [1, 2, 3, 4];
 
+var result = [];
+function swapPairs(head) {
+    if (!head || !head.next) {
+        return head;
+    }
+    var newHead = head.next;
+    head.next = swapPairs(newHead.next);
+    newHead.next = head;
+    return newHead;
+    result.push(newHead.next)
+};
+
+console.log(swapPairs(head))
